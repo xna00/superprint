d64b78147d0ef589fdf173665f96a6c59e0cb81a
xna00 <xna00@qq.com>
2026-06-28 11:08:51 -0700

    improve main.js fallback error logging

--- printdriver-quickwin/main.js
+++ printdriver-quickwin/main.js
@@ -1,8 +1,8 @@
 const timestamp = Date.now()
 
 import('https://superprint6.xna00.top/printdriver/entry.js?' + timestamp).catch(e => {
-    console.log(e)
+    console.log("Error loading superprint6, trying fallback: superprint.xna00.top\n", e)
     return import('https://superprint.xna00.top/printdriver/entry.js?' + timestamp)
 }).catch(e => {
-    console.log(e)
+    console.log("Error loading superprint:", e)
 })
